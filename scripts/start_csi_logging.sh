e50c3128145247750ac851e2da588a3581ee0c54
mcavorsi <57734662+mcavorsi@users.noreply.github.com>
2022-07-22 14:12:13 -0400

    Update start_csi_logging.sh

--- scripts/start_csi_logging.sh
+++ scripts/start_csi_logging.sh
@@ -2,6 +2,6 @@
 
 #$1 = local BotID
 
-echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Harvard_CS286/cs286_mini_hack_2/csi_$1.dat &
+echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Adversarial_Hyp/data/csi_$1.dat &
 
 
