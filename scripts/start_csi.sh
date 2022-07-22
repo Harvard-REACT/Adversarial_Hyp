846c36407b4d3c8e2632b7c4d1b073a940701001
mcavorsi <57734662+mcavorsi@users.noreply.github.com>
2022-07-22 14:11:50 -0400

    Update start_csi.sh

--- scripts/start_csi.sh
+++ scripts/start_csi.sh
@@ -3,7 +3,7 @@
 #$1 = local BotID
 #$2 = packet length, default = 29
 
-echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Harvard_CS286/cs286_mini_hack_2/csi_$1.dat &
+echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Adversarial_Hyp/data/csi_$1.dat &
 
 sleep 0.5
 
