b097fe32c07f7b0057d07d11fa317ffc355274e7
mcavorsi <57734662+mcavorsi@users.noreply.github.com>
2022-07-22 14:15:39 -0400

    Update stop_remote_csi.sh

--- scripts/stop_remote_csi.sh
+++ scripts/stop_remote_csi.sh
@@ -4,8 +4,14 @@
 #$2 = remote rx1 IP
 #$3 = remote tx1 username
 #$4 = remote tx1 IP
+#$5 = remote tx2 username
+#$6 = remote tx2 IP
+#$7 = remote tx3 username
+#$8 = remote tx3 IP
 
-ssh $1@$2 "bash ~/Harvard_CS286/cs286_mini_hack_2/scripts/stop_csi.sh"
-ssh $3@$4 "bash ~/Harvard_CS286/cs286_mini_hack_2/scripts/stop_csi.sh"
+ssh $1@$2 "bash ~/Adversarial_Hyp/scripts/stop_csi.sh"
+ssh $3@$4 "bash ~/Adversarial_Hyp/scripts/stop_csi.sh"
+ssh $5@$6 "bash ~/Adversarial_Hyp/scripts/stop_csi.sh"
+ssh $7@$8 "bash ~/Adversarial_Hyp/scripts/stop_csi.sh"
 
 
