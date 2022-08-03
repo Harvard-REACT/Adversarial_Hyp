85d70cf6cc30af94148e0ae0ad811ebb4a1b3360
mcavorsi <57734662+mcavorsi@users.noreply.github.com>
2022-08-03 17:00:32 -0400

    Update start_csi.sh

--- scripts/start_csi.sh
+++ scripts/start_csi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #$1 = local BotID
-#$2 = packet length, default = 29
+#$2 = number of tx robots
 
 echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Adversarial_Hyp/data/csi_$1.dat &
 
 #sleep 0.5
 
-echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/injection/random_packets 10000 $2 1 7500 & #Only send forward packets
+echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/injection_multiple/random_packets_multiple 10000 35 1 7500 $2 & #Only send forward packets
