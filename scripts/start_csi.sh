9ee51b7a788d71dc7b4c4412d5737f4ae2d0773d
mcavorsi <57734662+mcavorsi@users.noreply.github.com>
2022-08-03 14:25:21 -0400

    Update start_csi.sh

--- scripts/start_csi.sh
+++ scripts/start_csi.sh
@@ -5,6 +5,6 @@
 
 echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink/log_to_file ${HOME}/Adversarial_Hyp/data/csi_$1.dat &
 
-sleep 0.5
+#sleep 0.5
 
-echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/injection_bak/random_packets 10000 $2 1 7500 & #Only send forward packets
+echo "abc123" | sudo -S ${HOME}/WSR-Toolbox-linux-80211n-csitool-supplementary/injection/random_packets 10000 $2 1 7500 & #Only send forward packets
